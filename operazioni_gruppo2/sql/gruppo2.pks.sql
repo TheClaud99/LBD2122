4265886a5c60328b4e71e711dd277f0d608844b4
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2021-11-12 23:18:38 +0100

    Update gruppo2.pks.sql

--- operazioni_gruppo2/sql/gruppo2.pks.sql
+++ operazioni_gruppo2/sql/gruppo2.pks.sql
@@ -4,9 +4,9 @@ root constant VARCHAR2(125) := 'http://131.114.73.203:8080/apex/';
 user constant VARCHAR2(25) := 'nvetrini.gruppo2.';
 
 /* OPERAZIONI SULLE OPERE */
-/*procedure menuOpere (sessionID NUMBER DEFAULT NULL);
+procedure menuOpere (sessionID NUMBER DEFAULT NULL);
 PROCEDURE InserisciOpera(
-    sessionID NUMBER DEFAULT NULL 
+    sessionID NUMBER DEFAULT NULL
 );
 PROCEDURE ConfermaDatiOpera(
     sessionID NUMBER DEFAULT 0,
@@ -19,10 +19,36 @@ PROCEDURE InserisciDatiOpera(
     titolo VARCHAR2 DEFAULT 'Sconosciuto',
     anno VARCHAR2 DEFAULT NULL,
     idmusei NUMBER DEFAULT NULL
-);*/
+);
+
+ROCEDURE ModificaOpera(
+    sessionID NUMBER DEFAULT NULL,
+    operaID NUMBER DEFAULT 0,
+    titoloOpera VARCHAR2 DEFAULT 'Sconosciuto'
+);
+ 
+PROCEDURE ConfermaUpdateOpera(
+    sessionID NUMBER DEFAULT 0,
+    operaID NUMBER DEFAULT 0,
+    titolo VARCHAR2 DEFAULT 'Sconosciuto',
+    anno VARCHAR2 DEFAULT NULL,
+    fineperiodo VARCHAR2 DEFAULT NULL,
+    idmusei NUMBER DEFAULT 0
+);
+ 
+ 
+ 
+PROCEDURE UpdateOpera(
+    sessionID NUMBER DEFAULT 0,
+    operaID NUMBER DEFAULT 0,
+    newTitolo VARCHAR2 DEFAULT 'Sconosciuto',
+    newAnno VARCHAR2 DEFAULT 'Sconosciuto',
+    newFineperiodo NUMBER DEFAULT 0,
+    newIDmusei NUMBER DEFAULT 0
+);
+
 
 /* OPERAZIONI SUGLI AUTORI */
-/* Inserimento */
 procedure menuAutori (
     sessionID NUMBER DEFAULT NULL,
     authName VARCHAR2 DEFAULT NULL,
@@ -33,7 +59,7 @@ procedure menuAutori (
     );
 
 PROCEDURE InserisciAutore(
-	sessionID NUMBER DEFAULT NULL,
+    sessionID NUMBER DEFAULT NULL,
     authName VARCHAR2 DEFAULT NULL,
     authSurname VARCHAR2 DEFAULT NULL,
     dataNascita VARCHAR2 DEFAULT NULL,
@@ -42,65 +68,46 @@ PROCEDURE InserisciAutore(
 );
 
 PROCEDURE ConfermaDatiAutore(
-	sessionID NUMBER DEFAULT 0,
-	authName VARCHAR2 DEFAULT 'Sconosciuto',
-	authSurname VARCHAR2 DEFAULT 'Sconosciuto',
-	dataNascita varchar2 DEFAULT NULL,
-	dataMorte varchar2 DEFAULT NULL,
-	nation VARCHAR2 DEFAULT 'Sconosciuta'
+    sessionID NUMBER DEFAULT 0,
+    authName VARCHAR2 DEFAULT 'Sconosciuto',
+    authSurname VARCHAR2 DEFAULT 'Sconosciuto',
+    dataNascita varchar2 DEFAULT NULL,
+    dataMorte varchar2 DEFAULT NULL,
+    nation VARCHAR2 DEFAULT 'Sconosciuta'
 );
 
 PROCEDURE InserisciDatiAutore(
-	sessionID NUMBER DEFAULT 0,
-	authName VARCHAR2 DEFAULT 'Sconosciuto',
-	authSurname VARCHAR2 DEFAULT 'Sconosciuto',
-	dataNascita varchar2 DEFAULT NULL,
-	dataMorte varchar2 DEFAULT NULL,
-	nation VARCHAR2 DEFAULT 'Sconosciuta'
+    sessionID NUMBER DEFAULT 0,
+    authName VARCHAR2 DEFAULT 'Sconosciuto',
+    authSurname VARCHAR2 DEFAULT 'Sconosciuto',
+    dataNascita varchar2 DEFAULT NULL,
+    dataMorte varchar2 DEFAULT NULL,
+    nation VARCHAR2 DEFAULT 'Sconosciuta'
 );
-/* Visualizza: operazione = 0
-   Modifica: operazione = 1
-   Rimovi: operazione = 2
-*/
-PROCEDURE ModificaAutore(
-	sessionID NUMBER DEFAULT 0,
-	authorID NUMBER DEFAULT 0,
-	operazione NUMBER DEFAULT 0
-);
-
-PROCEDURE UpdateAutore(
-	sessionID NUMBER DEFAULT 0,
-	authID NUMBER DEFAULT 0,
-	newName VARCHAR2 DEFAULT 'Sconosciuto',
-	newSurname VARCHAR2 DEFAULT 'Sconosciuto',
-	newBirth VARCHAR2 DEFAULT NULL,
-	newDeath VARCHAR2 DEFAULT NULL,
-	newNation VARCHAR2 DEFAULT 'Sconosciuta'
-);
-
 
 /* OPERAZIONI SULLE DESCRIZIONI  */
-PROCEDURE InserisciDescrizione(
-	sessionID NUMBER DEFAULT NULL,
-    lingua VARCHAR2 DEFAULT NULL,
-    livello VARCHAR2 DEFAULT NULL,
-    testodescr VARCHAR2 DEFAULT NULL,
-    operaID NUMBER DEFAULT NULL);
+PROCEDURE InserisciDescrizione(sessionID NUMBER DEFAULT NULL);
 
 PROCEDURE ConfermaDatiDescrizione(
-	sessionID NUMBER DEFAULT 0,
-	lingua VARCHAR2 DEFAULT 'Sconosciuta',
-	livello VARCHAR2 DEFAULT 'Sconosciuto',
-	testodescr VARCHAR2 DEFAULT NULL,
-	operaID NUMBER DEFAULT NULL
+    sessionID NUMBER DEFAULT 0,
+    lingua VARCHAR2 DEFAULT 'Sconosciuta',
+    livello VARCHAR2 DEFAULT 'Sconosciuto',
+    testodescr VARCHAR2 DEFAULT NULL,
+    operaID NUMBER DEFAULT NULL
 );
 
 PROCEDURE InserisciDatiDescrizione(
-	sessionID NUMBER DEFAULT 0,
-	lingua VARCHAR2 DEFAULT 'Sconosciuta',
-	livello VARCHAR2 DEFAULT 'Sconosciuto',
-	testodescr VARCHAR2 DEFAULT NULL,
-	operaID NUMBER DEFAULT NULL
+    sessionID NUMBER DEFAULT 0,
+    lingua VARCHAR2 DEFAULT 'Sconosciuta',
+    livello VARCHAR2 DEFAULT 'Sconosciuto',
+    testodescr VARCHAR2 DEFAULT NULL,
+    operaID NUMBER DEFAULT NULL
+);
+
+PROCEDURE VisualizzaAutore(
+    sessionID NUMBER DEFAULT 0,
+    authorID NUMBER DEFAULT 0,
+    modifica NUMBER DEFAULT 0
 );
 
 END gruppo2;
